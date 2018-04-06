3cd11c051921de409391525f586ccc4329113423
Alexandre Bourget <alex@bourget.cc>
2018-04-06 17:08:22 -0400

    Added eos-hexpubkey

---
+++ cmd/eos-hexpubkey/README.md
@@ -0,0 +1,6 @@
+Print an EOS Public key as hex
+------------------------------
+
+This is a very stupid little program, uses the `eosapi` Go library to decode the Public key and print it out.
+
+This is mainly because the `regproducer` call requires a hex version of the public key.  Go figure.
---
+++ cmd/eos-hexpubkey/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"encoding/hex"
+	"flag"
+	"fmt"
+	"log"
+
+	"github.com/eosioca/eosapi/ecc"
+)
+
+func main() {
+	flag.Parse()
+
+	if len(flag.Args()) != 1 {
+		log.Fatalln("Please specify a public key")
+	}
+
+	textkey := flag.Args()[0]
+	pubkey, err := ecc.NewPublicKey(textkey)
+	if err != nil {
+		log.Fatalln("invalid public key:", err)
+	}
+
+	fmt.Printf("public key in hex form: %q\n", hex.EncodeToString(pubkey))
+}
