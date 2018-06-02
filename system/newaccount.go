25361172f785b44c1c04726c30546e61daf20aab
Alexandre Bourget <alex@bourget.cc>
2018-06-02 17:15:59 -0400

    Added delegated account creation.

--- system/newaccount.go
+++ system/newaccount.go
@@ -41,12 +41,55 @@ func NewNewAccount(creator, newAccount eos.AccountName, publicKey ecc.PublicKey)
 	}
 }
 
+// NewDelegatedNewAccount returns a `newaccount` action that lives on the
+// `eosio.system` contract. It is filled with an authority structure that
+// delegates full control of the new account to an already existing account.
+func NewDelegatedNewAccount(creator, newAccount eos.AccountName, delegatedTo eos.AccountName) *eos.Action {
+	return &eos.Action{
+		Account: AN("eosio"),
+		Name:    ActN("newaccount"),
+		Authorization: []eos.PermissionLevel{
+			{Actor: creator, Permission: PN("active")},
+		},
+		ActionData: eos.NewActionData(NewAccount{
+			Creator: creator,
+			Name:    newAccount,
+			Owner: eos.Authority{
+				Threshold: 1,
+				Keys:      []eos.KeyWeight{},
+				Accounts: []eos.PermissionLevelWeight{
+					eos.PermissionLevelWeight{
+						Permission: eos.PermissionLevel{
+							Actor:      delegatedTo,
+							Permission: PN("active"),
+						},
+						Weight: 1,
+					},
+				},
+			},
+			Active: eos.Authority{
+				Threshold: 1,
+				Keys:      []eos.KeyWeight{},
+				Accounts: []eos.PermissionLevelWeight{
+					eos.PermissionLevelWeight{
+						Permission: eos.PermissionLevel{
+							Actor:      delegatedTo,
+							Permission: PN("active"),
+						},
+						Weight: 1,
+					},
+				},
+			},
+		}),
+	}
+}
+
 // NewAccount represents a `newaccount` action on the `eosio.system`
 // contract. It is one of the rare ones to be hard-coded into the
 // blockchain.
 type NewAccount struct {
-	Creator  eos.AccountName `json:"creator"`
-	Name     eos.AccountName `json:"name"`
-	Owner    eos.Authority   `json:"owner"`
-	Active   eos.Authority   `json:"active"`
+	Creator eos.AccountName `json:"creator"`
+	Name    eos.AccountName `json:"name"`
+	Owner   eos.Authority   `json:"owner"`
+	Active  eos.Authority   `json:"active"`
 }
