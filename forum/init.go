f320fb22d1a5c19ad599d7ee82d7ef6ba31372c1
Alexandre Bourget <alex@bourget.cc>
2018-07-10 11:21:32 -0400

    No double pointers plz.

--- forum/init.go
+++ forum/init.go
@@ -3,9 +3,9 @@ package forum
 import eos "github.com/eoscanada/eos-go"
 
 func init() {
-	eos.RegisterAction(AN("eosforumtest"), ActN("post"), &Post{})
-	eos.RegisterAction(AN("eosforumtest"), ActN("vote"), &Vote{})
-	eos.RegisterAction(AN("eosforumtest"), ActN("remove"), &Remove{})
+	eos.RegisterAction(AN("eosforumtest"), ActN("post"), Post{})
+	eos.RegisterAction(AN("eosforumtest"), ActN("remove"), Remove{})
+	eos.RegisterAction(AN("eosforumtest"), ActN("vote"), Vote{})
 }
 
 var AN = eos.AN
