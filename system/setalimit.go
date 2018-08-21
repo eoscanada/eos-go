8173b8c4269f5a1f79671e95cd67c730954b95a7
Alexandre Bourget <alex@bourget.cc>
2018-08-21 19:20:35 -0400

    Whoops, setalimit_s_

--- system/setalimit.go
+++ system/setalimit.go
@@ -4,15 +4,15 @@ import (
 	eos "github.com/eoscanada/eos-go"
 )
 
-// NewSetalimit sets the account limits. Requires signature from `eosio@active` account.
-func NewSetalimit(account eos.AccountName, ramBytes, netWeight, cpuWeight int64) *eos.Action {
+// NewSetalimits sets the account limits. Requires signature from `eosio@active` account.
+func NewSetalimits(account eos.AccountName, ramBytes, netWeight, cpuWeight int64) *eos.Action {
 	a := &eos.Action{
 		Account: AN("eosio"),
 		Name:    ActN("setalimit"),
 		Authorization: []eos.PermissionLevel{
 			{Actor: eos.AccountName("eosio"), Permission: PN("active")},
 		},
-		ActionData: eos.NewActionData(Setalimit{
+		ActionData: eos.NewActionData(Setalimits{
 			Account:   account,
 			RAMBytes:  ramBytes,
 			NetWeight: netWeight,
@@ -22,8 +22,8 @@ func NewSetalimit(account eos.AccountName, ramBytes, netWeight, cpuWeight int64)
 	return a
 }
 
-// Setalimit represents the `eosio.system_contract::setalimit` action.
-type Setalimit struct {
+// Setalimits represents the `eosio.system_contract::setalimit` action.
+type Setalimits struct {
 	Account   eos.AccountName `json:"account"`
 	RAMBytes  int64           `json:"ram_bytes"`
 	NetWeight int64           `json:"net_weight"`
