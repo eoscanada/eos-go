c41e6f6831c66f0aff02933a13c5a135b7230aff
billettc <charles@dix975.com>
2018-08-27 12:01:21 -0400

    Add param to main

--- cmd/eos-p2p-proxy/main.go
+++ cmd/eos-p2p-proxy/main.go
@@ -1,18 +1,23 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 
 	"github.com/eoscanada/eos-go/p2p"
 )
 
-func main() {
+var peer1 = flag.String("peer1", "localhost:9876", "peer 1")
+var peer2 = flag.String("peer2", "localhost:2222", "peer 1")
+var chainID = flag.String("chain-id", "cf057bbfb72640471fd910bcb67639c22df9f92470936cddc1ade0e2f2e7dc4f", "peer 1")
 
+func main() {
+	flag.Parse()
 	//chainID, err := hex.DecodeString("aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906")
 
 	proxy := p2p.NewProxy(
-		p2p.NewOutgoingPeer("localhost:9876", "eos-proxy"),
-		p2p.NewOutgoingPeer("localhost:2222", "eos-proxy"),
+		p2p.NewOutgoingPeer(*peer1, "eos-proxy"),
+		p2p.NewOutgoingPeer(*peer2, "eos-proxy"),
 	)
 
 	//proxy := p2p.NewProxy(
@@ -26,7 +31,6 @@ func main() {
 	//)
 
 	proxy.RegisterHandler(p2p.StringLoggerHandler)
-	//fmt.Println(proxy.ConnectAndStart("aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906"))
-	fmt.Println(proxy.ConnectAndStart("cf057bbfb72640471fd910bcb67639c22df9f92470936cddc1ade0e2f2e7dc4f"))
+	fmt.Println(proxy.ConnectAndStart(*chainID))
 
 }
