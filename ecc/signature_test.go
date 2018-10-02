2b56b20d54df2cb520e4697ae30fff968d715fcb
Alexandre Bourget <alex@bourget.cc>
2018-10-02 19:15:03 -0400

    Integrate the tests from atticlabs, see how they behave here.

--- ecc/signature_test.go
+++ ecc/signature_test.go
@@ -11,7 +11,42 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func FixmeTestSignatureSerialization(t *testing.T) {
+func TestEOSJSEquiv(t *testing.T) {
+	// Test cases from eosjs (src/signature.test.js)
+	// 	    const digest = '6cb75bc5a46a7fdb64b92efefca01ed7b060ab5e0d625226e8efbc0980c3ddc1'
+	// const wif = '5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3'
+	// const expectedSig = 'SIG_K1_Kk1yUXAG2Cfo2qvWuJiyvaGdwZBQ1HzSf4EZ9arUTWBL4kTngLM1GSUU59bJUVAqwJ886CNQMcR7mmx323gjQGvhEU8WpX'
+
+	privKey, err := NewPrivateKey("5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3")
+	require.NoError(t, err)
+
+	digest, err := hex.DecodeString("6cb75bc5a46a7fdb64b92efefca01ed7b060ab5e0d625226e8efbc0980c3ddc1")
+	require.NoError(t, err)
+
+	sig, err := privKey.Sign(digest)
+	require.NoError(t, err)
+
+	assert.Equal(t, "SIG_K1_Kk1yUXAG2Cfo2qvWuJiyvaGdwZBQ1HzSf4EZ9arUTWBL4kTngLM1GSUU59bJUVAqwJ886CNQMcR7mmx323gjQGvhEU8WpX", sig.String())
+}
+
+func TestEOSJSEquivEmptyDigest(t *testing.T) {
+	// Test cases from eosjs (src/signature.test.js)
+	// const digest = Buffer.alloc(32, 0)
+	// const wif = '5HxQKWDznancXZXm7Gr2guadK7BhK9Zs8ejDhfA9oEBM89ZaAru'
+	// const expectedSig = 'SIG_K1_Jz9d1rKmMV51EY6dnU3pNaDiLvGTeVdxDZGvJEfAkdcwzs97gNg5yYPhPSdEg33Jyp5736Tnnzccf1p6h6vedXpHSUBio1'
+	privKey, err := NewPrivateKey("5HxQKWDznancXZXm7Gr2guadK7BhK9Zs8ejDhfA9oEBM89ZaAru")
+	require.NoError(t, err)
+
+	digest, err := hex.DecodeString("0000000000000000000000000000000000000000000000000000000000000000")
+	require.NoError(t, err)
+
+	sig, err := privKey.Sign(digest)
+	require.NoError(t, err)
+
+	assert.Equal(t, "SIG_K1_Jz9d1rKmMV51EY6dnU3pNaDiLvGTeVdxDZGvJEfAkdcwzs97gNg5yYPhPSdEg33Jyp5736Tnnzccf1p6h6vedXpHSUBio1", sig.String())
+}
+
+func TestSignatureSerialization(t *testing.T) {
 	privkey, err := NewPrivateKey("5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3")
 	require.NoError(t, err)
 
