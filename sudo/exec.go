4f022e5df363f75e94488a16d4e639a660b8ca2e
Francois Proulx <francois@eoscanada.com>
2018-11-08 15:35:13 -0500

    Updated sudoo

--- sudo/exec.go
+++ sudo/exec.go
@@ -9,7 +9,7 @@ import (
 //
 // Given an `eos.Transaction`, call `eos.MarshalBinary` on it first,
 // pass the resulting bytes as `eos.HexBytes` here.
-func NewExec(executer eos.AccountName, transaction eos.HexBytes) *eos.Action {
+func NewExec(executer eos.AccountName, transaction eos.Transaction) *eos.Action {
 	a := &eos.Action{
 		Account: eos.AccountName("eosio.wrap"),
 		Name:    eos.ActionName("exec"),
@@ -27,5 +27,5 @@ func NewExec(executer eos.AccountName, transaction eos.HexBytes) *eos.Action {
 // Exec represents the `eosio.system::exec` action.
 type Exec struct {
 	Executer    eos.AccountName `json:"executer"`
-	Transaction eos.HexBytes    `json:"trx"`
+	Transaction eos.Transaction `json:"trx"`
 }
