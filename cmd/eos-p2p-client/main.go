94e0cb2228638c2674581c82404053c8699fd527
fanyang <fanyang.coder@gmail.com>
2018-11-25 19:22:32 +0800

    use -v to show log from p2p in tools in cmd

--- cmd/eos-p2p-client/main.go
+++ cmd/eos-p2p-client/main.go
@@ -11,13 +11,18 @@ import (
 
 var peer = flag.String("peer", "localhost:9876", "peer to connect to")
 var chainID = flag.String("chain-id", "cf057bbfb72640471fd910bcb67639c22df9f92470936cddc1ade0e2f2e7dc4f", "net chainID to connect to")
+var showLog = flag.Bool("v", false, "show detail log")
 
 func main() {
 	flag.Parse()
 
+	if *showLog {
+		p2p.EnableP2PLogging()
+	}
+	defer p2p.SyncLogger()
+
 	cID, err := hex.DecodeString(*chainID)
 	if err != nil {
-
 		log.Fatal(err)
 	}
 
