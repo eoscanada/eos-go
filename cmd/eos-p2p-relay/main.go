659f5e08b395ec3c480f356ef723e527f3f778de
fanyang <fanyang.coder@gmail.com>
2018-11-25 19:22:32 +0800

    use -v to show log from p2p in tools in cmd

--- cmd/eos-p2p-relay/main.go
+++ cmd/eos-p2p-relay/main.go
@@ -10,14 +10,17 @@ import (
 
 var peer = flag.String("peer", "", "peer")
 var listeningAddress = flag.String("listening-address", "", "address on with the relay will listen")
+var showLog = flag.Bool("v", false, "show detail log")
 
 func main() {
-
 	flag.Parse()
 
+	if *showLog {
+		p2p.EnableP2PLogging()
+	}
+	defer p2p.SyncLogger()
+
 	relay := p2p.NewRelay(*listeningAddress, *peer)
-
 	relay.RegisterHandler(p2p.StringLoggerHandler)
-
 	fmt.Println(relay.Start())
 }
