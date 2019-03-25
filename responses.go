a02722934ca1e428382e6b0725de951d93541d8e
Alexey Usachev <ff255@outlook.com>
2019-03-25 21:38:12 +0300

    delete duplicate code

--- responses.go
+++ responses.go
@@ -432,14 +432,6 @@ type ControlledAccountsResp struct {
 	ControlledAccounts []string `json:"controlled_accounts"`
 }
 
-type KeyAccountsResp struct {
-	AccountNames []string `json:"account_names`
-}
-
-type ControlledAccountsResp struct {
-	ControlledAccounts []string `json:"controlled_accounts`
-}
-
 type GetCurrencyStatsResp struct {
 	Supply    Asset       `json:"supply"`
 	MaxSupply Asset       `json:"max_supply"`
