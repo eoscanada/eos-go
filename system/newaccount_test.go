b8944db8b194db8afb403945063caaf4ace38886
Ivan Kuznetsov <me@jeiwan.ru>
2019-04-22 19:59:15 +0700

    Set empty accounts and waits in TestActionNewAccount

--- system/newaccount_test.go
+++ system/newaccount_test.go
@@ -37,6 +37,8 @@ func TestActionNewAccount(t *testing.T) {
 						Weight:    1,
 					},
 				},
+				Accounts: []eos.PermissionLevelWeight{},
+				Waits:    []eos.WaitWeight{},
 			},
 			Active: eos.Authority{
 				Threshold: 1,
@@ -46,6 +48,8 @@ func TestActionNewAccount(t *testing.T) {
 						Weight:    1,
 					},
 				},
+				Accounts: []eos.PermissionLevelWeight{},
+				Waits:    []eos.WaitWeight{},
 			},
 		}),
 	}
@@ -66,7 +70,7 @@ func TestActionNewAccount(t *testing.T) {
 	buf, err = json.Marshal(a.ActionData.Data)
 	assert.NoError(t, err)
 
-	assert.Equal(t, "{\"creator\":\"eosio\",\"name\":\"abourget\",\"owner\":{\"threshold\":1,\"keys\":[{\"key\":\"EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV\",\"weight\":1}]},\"active\":{\"threshold\":1,\"keys\":[{\"key\":\"EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV\",\"weight\":1}]}}", string(buf))
+	assert.Equal(t, "{\"creator\":\"eosio\",\"name\":\"abourget\",\"owner\":{\"threshold\":1,\"keys\":[{\"key\":\"EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV\",\"weight\":1}],\"accounts\":[],\"waits\":[]},\"active\":{\"threshold\":1,\"keys\":[{\"key\":\"EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV\",\"weight\":1}],\"accounts\":[],\"waits\":[]}}", string(buf))
 	// 00096e88 0000 0000 00000000 00 00 00 00 01 0000000000ea3055
 
 	// WUTz that ?
