9837ac22bc37e61fa0a232d1ec58a0528eb4897f
Stéphane Duchesneau <stephane.duchesneau@eoscanada.com>
2019-09-19 11:50:27 -0400

    add BlockState model to p2ptypes

--- p2ptypes.go
+++ p2ptypes.go
@@ -212,6 +212,46 @@ type ProducerSchedule struct {
 	Producers []ProducerKey `json:"producers"`
 }
 
+type MerkleRoot struct {
+	ActiveNodes []string `json:"_active_nodes"`
+	NodeCount   uint32   `json:"_node_count"`
+}
+
+type EOSNameOrUint32 interface{}
+
+type BlockState struct {
+	BlockID                          string              `json:"id"`
+	BlockNum                         string              `json:"block_num"`
+	DPoSProposedIrreversibleBlockNum uint32              `json:"dpos_proposed_irreversible_blocknum"`
+	DPoSIrreversibleBlockNum         uint32              `json:"dpos_irreversible_blocknum"`
+	ActiveSchedule                   *ProducerSchedule   `json:"active_schedule"`
+	BlockrootMerkle                  *MerkleRoot         `json:"blockroot_merkle"`
+	ProducerToLastProduced           []EOSNameOrUint32   `json:"producer_to_last_produced"`
+	ProducerToLastIRB                []EOSNameOrUint32   `json:"producer_to_last_implied_irb"`
+	BlockSigningKey                  ecc.PublicKey       `json:"block_signing_key"`
+	ConfirmCount                     []uint32            `json:"confirm_count"`
+	PendingSchedule                  *PendingSchedule    `json:"pending_schedule"`
+	ActivatedProtocolFeatures        map[string]HexBytes `json:"activated_protocol_features"`
+	SignedBlock                      *SignedBlock        `json:"signed_block"`
+	Validated                        bool                `json:"validated"`
+}
+
+type PendingSchedule struct {
+	ScheduleLIBNum uint32            `json:"schedule_lib_num"`
+	ScheduleHash   HexBytes          `json:"schedule_hash"`
+	Schedule       *ProducerSchedule `json:"producer_schedule"`
+}
+
+func (b *SignedBlock) ToHeader() *BlockHeader {
+	return &BlockHeader{
+		Timestamp: b.Timestamp,
+	}
+}
+
+func (b *BlockState) Header() *BlockHeader {
+	return b.SignedBlock.ToHeader()
+}
+
 type BlockHeader struct {
 	Timestamp        BlockTimestamp            `json:"timestamp"`
 	Producer         AccountName               `json:"producer"`
