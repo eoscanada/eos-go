1d323b9dd334a40081c55f0d63f0fb65e00c2a27
Julien Cassis <juliencassis@gmail.com>
2020-07-23 12:47:50 -0400

    Added init system func

---
+++ system/initsystem.go
@@ -0,0 +1,30 @@
+package system
+
+import (
+	eos "github.com/eoscanada/eos-go"
+)
+
+// NewInitSystem returns a `init` action that lives on the
+// `eosio.system` contract.
+func NewInitSystem(version eos.Varuint32, core eos.Symbol) *eos.Action {
+	return &eos.Action{
+		Account: AN("eosio"),
+		Name:    ActN("regproducer"),
+		Authorization: []eos.PermissionLevel{
+			{
+				Actor:      AN("eosio"),
+				Permission: eos.PermissionName("active"),
+			},
+		},
+		ActionData: eos.NewActionData(Init{
+			Version: version,
+			Core:    core,
+		}),
+	}
+}
+
+// Init represents the `eosio.system::init` action
+type Init struct {
+	Version eos.Varuint32 `json:"version"`
+	Core    eos.Symbol    `json:"core"`
+}
