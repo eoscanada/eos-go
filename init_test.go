bb75101dc2f674c97e40d2b298fde20d2c1f64ca
Henrik Hautakoski <henrik@eossweden.org>
2023-04-13 17:46:40 +0200

    init_test.go: fixate timezone for tests. (#207)
    All tests that deals with dates expects the timezone to be in America/New_York.

--- init_test.go
+++ init_test.go
@@ -1,9 +1,13 @@
 package eos
 
 import (
+	"time"
+
 	"github.com/streamingfast/logging"
 )
 
 func init() {
 	logging.InstantiateLoggers()
+
+	time.Local, _ = time.LoadLocation("America/New_York")
 }
